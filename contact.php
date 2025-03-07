2c4791ac0b41b352b09fbb447ed552b43cd1442d
fabricetfab <fabricetfab@gmail.com>
2025-03-07 23:29:53 +0200

    new

---
+++ contact.php
@@ -0,0 +1,235 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="contact.css">
+  <title>Lk Group | Home Page</title>
+</head>
+<body>
+
+  <div class="navigation-container p-3">
+    <img src="imgs/logo.png" alt="logo" class="logo" />
+    
+    <div class="header-navigation">
+      <a href="home.php">Home</a>
+      <a href="contacts.php" class="active">contacts</a>
+      <a href="about.php">About Us</a>
+      <a href="contact.php">Contact Us</a>
+    </div>
+
+    <div class="header-links">
+      <svg xmlns="http://www.w3.org/10000/svg" class="header-search" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+        <circle cx="11" cy="11" r="8"></circle>
+        <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
+      </svg>
+      <a href="/how-it-works">How it Works</a>
+      <a href="/faqs">FAQs</a>
+    </div>
+  </div>
+  <div class="contacts-container">
+    <div class="contacts-hero">
+      <img src="imgs/contacts-bg.jfif" class="img-fluid">
+      <div class="hero-contents">
+        <h1>CONTACT <span>US</span></h1>
+        <p>“We understand that success in the digital world requires a holistic approach so Lk group is a Multifaceted Approach to Business Growth”</p>
+      </div>
+    </div>
+  </div>
+
+
+  <div class="contacts-contents">
+    <img src="imgs/new-big-bg.png" class="img-fluid contacts-contents-bg">
+    <div class="container-xlg one text-center">
+      <h1 class="pt-4 h3" style="color: #2BADE2;">We’d Love to Hear from You!</h1>
+      <p class="text-light mt-5 m-auto fs-6" style="max-width: 768px;opacity: .7;">we're here to help! Our team at LK GROUP is just a message away. Fill out the form below, and let's connect to make your vision a reality.</p>
+      <p class="text-light mt-5 m-auto fs-6">Your next step toward growth starts here—let's chat!</p>
+      <div class="container-sm border-bottom mt-5 border-dark"></div>
+    </div>
+
+     <div class="contact">
+        <div class="contact-contents">
+          <div class="icons">
+            <button class="d-block"><img src="icons/linkedin.svg" class="d-block"></button>
+            <button class="d-block"><img style="height: 20px;" src="icons/face.svg" class="d-block"></button>
+            <button class="d-block"><img src="icons/insta.svg" class="d-block"></button>
+            <button class="d-block"><img src="icons/x.svg" class="d-block"></button>
+          </div>
+          <div class="contact-info">
+            <h4><span>We are</span> open for any Suggestion by any mean of <span>communication.</span></h4>
+            <div class="contact-item">
+              <img src="icons/phone.svg">
+              <div>
+                <p>(+250)789486046</p>
+                <p>(+250)789486046</p>
+              </div>
+            </div>
+            <div class="contact-item">
+              <img src="icons/sms.svg">
+              <div>
+                <p>lkgroupworld@gmail.com</p>
+                <p>contact@lkrecruitmentrw.com</p>
+              </div>
+            </div>
+            <div class="contact-item">
+              <img src="icons/location.svg">
+              <div>
+                <p>4th floor La Bonne Addresse House, </p>
+                <p>Nyarugenge Kiyovu, Kigali City</p>
+              </div>
+            </div>
+            <div class="contact-item">
+              <img src="icons/time.svg">
+              <div>
+                <p>Opening time</p>
+                <p>Mon - Fri (9:00 AM - 17:00 PM)</p>
+              </div>
+            </div>
+            <div class="contact-item">
+              <button>See location on map<svg class="ms-3" stroke-width="1.8" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-down-right" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M14 13.5a.5.5 0 0 1-.5.5h-6a.5.5 0 0 1 0-1h4.793L2.146 2.854a.5.5 0 1 1 .708-.708L13 12.293V7.5a.5.5 0 0 1 1 0z"/>
+</svg></button>
+            </div>
+          </div>
+          <div class="contact-form">
+            <h3>Message <span>US !</span></h3>
+            <form>
+              <div>
+                <label class="mb-2 mt-2">Name</label>
+                <input type="text" name="" placeholder="Name">
+              </div>
+              <div>
+                <label class="mb-2 mt-2">Email</label>
+                <input type="email" name="" placeholder="Email">
+              </div>
+              <div>
+                <label class="mb-2 mt-2">Subject</label>
+                <input type="text" name="" placeholder="Subject">
+              </div>
+              <div>
+                <label class="mb-2 mt-2">Service</label>
+                <input type="text" name="" placeholder="Service">
+              </div>
+              <div class="fortextarea">
+                <label class="mb-2 mt-2">Message</label>
+                <textarea placeholder="Type a message..."></textarea>
+              </div>
+              <button class="send">Send</button>
+            </form>
+          </div>
+        </div>
+      </div>
+
+
+      <div class="location container-xlg">
+        <h1 class="h3 my-5">Location on map</h1>
+        <div class="location-contents">
+          <div class="">
+            <img src="imgs/location.png" class="img-fluid w-100">
+          </div>
+          <div class="">
+            <h2 class="h5 d-block"><span class="text-light">See Place</span> photos</h2>
+            <img src="imgs/location1.png" class="img-fluid w-100 d-block">
+            <img src="imgs/location2.png" class="img-fluid w-100 d-block">
+            <img src="imgs/location3.png" class="img-fluid w-100 d-block">
+          </div>
+        </div>
+      </div>
+
+
+      <div class="location-text row">
+        <div class="col">
+            <div class="contact-item">
+              <img src="icons/phone.svg">
+              <div>
+                <p>(+250)789486046</p>
+                <p>(+250)789486046</p>
+              </div>
+            </div>
+            <div class="contact-item">
+              <img src="icons/sms.svg">
+              <div>
+                <p>lkgroupworld@gmail.com</p>
+                <p>contact@lkrecruitmentrw.com</p>
+              </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="contact-item">
+              <img src="icons/location.svg">
+              <div>
+                <p>4th floor La Bonne Addresse House, </p>
+                <p>Nyarugenge Kiyovu, Kigali City</p>
+              </div>
+            </div>
+            <div class="contact-item">
+              <img src="icons/time.svg">
+              <div>
+                <p>Opening time</p>
+                <p>Mon - Fri (9:00 AM - 17:00 PM)</p>
+              </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="partners pb-5">
+          <h1>OUR PARTNERS</h1>
+          <div>
+            <img src="imgs/about3.png">
+            <img src="imgs/img1.jfif">
+            <img src="imgs/about3.png">
+            <img src="imgs/about3.png">
+            <img src="imgs/img1.jfif">
+            <img src="imgs/about3.png">
+          </div>  
+        </div>
+
+
+
+
+
+
+
+
+  </div>
+
+  <div class="footer">
+        <div>
+          <img src="imgs/logo1.png" alt="logo">
+          <h5>LK GROUP</h5>
+        </div>
+        <div>
+          <h4>Resources</h4>
+          <a href="#" class="active">Home</a>
+          <a href="#">About</a>
+          <a href="#">contacts</a>
+          <a href="#">Contact</a>
+        </div>
+        <div>
+          <h4>Support</h4>
+          <a href="#">FAQs</a>
+          <a href="#">Help</a>
+        </div>
+        <div>
+          <h4>Company</h4>
+          <a href="#">Our Mission</a>
+          <a href="#">Our Objective</a>
+          <a href="#">Our Values</a>
+          <a href="#">Our Team</a>
+        </div>
+        <div>
+          <h4>Useful Links</h4>
+          <a href="#">How we work</a>
+          <a href="#">What we do</a>
+        </div>
+        <div>
+          <h4>Social</h4>
+          <div class="footer-social">
+            <a href="#"><img src="icons/linkedin.svg"></a>
+            <a href="#"><img src="icons/x.svg"><a>
+            <a href="#"><img src="icons/insta.svg"></</a>
+          </div>
+        </div>
+</body>
+</html>
